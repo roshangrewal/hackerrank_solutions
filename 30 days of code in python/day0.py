96960d2432256047b2d79310e3d216c575d96c9f
roshangrewal <roshangrewal@hotmail.com>
2019-04-18 21:18:50 +0530

    Day 0 Code, Just For Fun

--- 30 days of code in python/day0.py
+++ 30 days of code in python/day0.py
@@ -3,5 +3,5 @@ input_string = input()
 
 # Print a string literal saying "Hello, World." to stdout.
 print('Hello, World.')
-
+#JustForFun
 # TODO: Write a line of code here that prints the contents of input_string to stdout.
